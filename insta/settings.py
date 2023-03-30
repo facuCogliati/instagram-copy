e370fd1fcf77639bcaa48090071a139db5bd92e1
Facundo Cogliati <facu.cogliati@gmail.com>
2023-03-30 20:41:07 -0300

    fix

--- insta/settings.py
+++ insta/settings.py
@@ -137,15 +137,12 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR / "static"),
 ]
 
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/images/'
 MEDIA_ROOT = 'static/images/'
 
-if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
-    # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # Turn on WhiteNoise storage backend that takes care of compressing static files
-    # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
