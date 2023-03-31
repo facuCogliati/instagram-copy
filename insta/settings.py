7268acc0b8d12e47c2e6991402f7fbeef8df3a65
Facundo Cogliati <facu.cogliati@gmail.com>
2023-03-30 21:15:43 -0300

    fixed

--- insta/settings.py
+++ insta/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -87,11 +87,18 @@ WSGI_APPLICATION = 'insta.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#     default='sqlite:///db.sqlite3',        
+#     conn_max_age=600 
+#     )    
+# }
+
 DATABASES = {
-    'default': dj_database_url.config(
-    default='sqlite:///db.sqlite3 ',        
-    conn_max_age=600 
-    )    
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
